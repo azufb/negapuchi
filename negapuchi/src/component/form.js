96508c91f6edfe7a2f2435a2a547ffdbb0f95314
Azusa Okamoto <curiousazunas@gmail.com>
2021-02-21 20:26:29 +0900

    入力できる状態にした。

--- negapuchi/src/component/form.js
+++ negapuchi/src/component/form.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
 class Form extends React.Component {
+    constructor(props) {
+        // 初期化
+        super(props);
+        this.state = {
+            content: ''
+        }
+    }
+
+    handleChange(event) {
+        let inputContent = event.target.value;
+        this.setState({
+            content: inputContent
+        })
+    }
     render() {
         return (
             <div>
                 <p>Formはここです。</p>
                 <form>
-                    <input type='text' />
+                    <input type='text' onChange={(event) => {this.handleChange(event)}} 
+                    value={ this.state.content } />
                     <input type='submit' value='変換' />
                 </form>
             </div>
@@ -15,3 +30,9 @@ class Form extends React.Component {
 }
 
 export default Form;
+
+/* extendsを用いて、Reactが用意しているAPIの、React.Componentを継承し、
+親子関係を持つclassを作成。
+継承した親classのプロパティを初期化するためには、constructorには、
+親classを表す、super()の引数として、初期化したい値(大抵は、props)を渡す。
+*/
