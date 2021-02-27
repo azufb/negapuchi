3fa414618c19a27d8a1f9a15a8932116d404533d
Azusa Okamoto <curiousazunas@gmail.com>
2021-02-27 14:06:17 +0900

    NGワードの設定と変換機能作成

--- negapuchi/src/component/form.js
+++ negapuchi/src/component/form.js
@@ -24,8 +24,20 @@ class Form extends React.Component {
         「又は」を表すのは、「|」。
         ピリオドは、そのまま入れると、改行文字以外のどの 1 文字にもマッチする、「特殊文字」だと認識されてしまうため。エスケープする必要がある。
         */
+
+
         let reg = new RegExp(/。|\./, 'gim');
         let regResult = this.state.content.replace(reg, '！');
+
+        let meireiList = [
+            'ろよ',
+            'ろや',
+            'せいや',
+            'せんかい',
+            'やれよ',
+            'やれや',
+            'やらんかい'
+        ]
 
         let onegai = [
             'て頂ければ嬉しいです！',
@@ -34,16 +46,38 @@ class Form extends React.Component {
             'てもらえたらありがたい！',
             'て欲しいです！'
         ]
-        let reg2 = this.state.content.match(/ろよ/)
 
-        if (reg2!== null) {
-            regResult = this.state.content.replace(reg2, onegai[Math.floor(Math.random()*onegai.length)])
+        for (let meirei of meireiList) {
+            let reg2 = this.state.content.match(meirei);
+
+            if (reg2 !== null) {
+                regResult = this.state.content.replace(reg2, onegai[Math.floor(Math.random()*onegai.length)])
+            }
+        }
+
+        let ngList = [
+            'しね',
+            '死ね',
+            'ころす',
+            '殺す',
+            'なぐる',
+            'なぐん',
+            '殴る',
+        ]
+
+        for (const element of ngList) {
+            let reg3 = this.state.content.match(element);
+
+            if (reg3 !== null) {
+                regResult = this.state.content.replace(reg3, '×××');
+            }
         }
 
         this.setState({
             lists: [...this.state.lists, regResult],
             content: ''
         });
+        
     }
 
     render() {
